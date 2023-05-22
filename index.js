e21806d4dbbed1effab6c39fe44f0cf7845ddc40
Vijin Vinod <vfcvijin@gmail.com>
2023-05-22 17:08:42 +0530

    hai

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const app = express();
 
+const cors = require('cors');
+
+app.use(cors());
+
 app.get('https://api-test1-5a9d.onrender.com', (req, res) => {
+    
     res.end("APi done")
 })
 
