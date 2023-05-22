e116893954237a2cd1cb8d38d79518f6d7956870
Vijin Vinod <vfcvijin@gmail.com>
2023-05-22 20:16:49 +0530

    Db added

--- index.js
+++ index.js
@@ -1,13 +1,37 @@
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
+const user = require("./modal")
+
 
 const cors = require('cors');
 
 app.use(cors());
 
 app.get('/', (req, res) => {
+
+    const user1 = user.find()
     
-    res.end("APi done")
+    res.end(user1)
 })
+
+app.post('/', async (req, res) => {
+
+    const {name} = req.body
+
+    const user1 = new user({
+        name: name
+    })
+
+    await user1.save()
+
+    res.end(user1,"added successfully")
+
+})
+
+
+
+mongoose.connect("mongodb+srv://vfcvijin:111@new.j2g0m0h.mongodb.net/").then(()=>console.log("database connected"))
 
 app.listen(4000,()=>console.log("The Server is  Running"));
